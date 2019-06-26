03e243f53ddde18b61b9d28a0155f04deccad306
ngo tung <ngovantungcoi@gmail.com>
2019-06-26 13:46:56 +0700

    baitap

---
+++ index.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+
+inputHeight = prompt("Enter the width ")
+inputWidth = prompt("Enter the height ")
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
